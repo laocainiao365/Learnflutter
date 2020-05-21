7ec041fa6f6d8826fad5ce4ddd32f7a1ca2743d7
study-flutter <1020318006@qq.com>
2020-05-21 13:25:25 +0800

    change scaffold readme

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,12 @@
 import 'package:flutter/material.dart';
-import 'HomeContent.dart';
-
+import './MaterialApp/index.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: Colors.black,
-      ),
-      home: HomeContent(),
-    );
+    return LcnMaterialApp();
   }
 }
 
